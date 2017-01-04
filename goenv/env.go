6a49eb822dc814826455d7a67b4c98d747b9d0c9
fx19800215@163.com <Fx19800215>
2017-01-04 11:52:32 +0800

    11

--- goenv/env.go
+++ goenv/env.go
@@ -1,6 +1,8 @@
 package goenv
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -15,20 +17,50 @@ var AppConstant map[string]string = make(map[string]string)
 func InitApp() {
 
 	//1、接收启动参数
-	appDir := os.Args[0]
-	AppConstant["APP_DIR"] = appDir //系统目录
+
+	appDir := flag.String("appdir", "", "系统启动目录")
+	flag.Parse()
+
+	AppConstant["APP_DIR"] = *appDir //系统目录
+
+	fmt.Println(" 当前项目路径    " + *appDir)
 
 	//读取配置文件
-	confdir := appDir + "/cfg/config.json"
+	confdir := *appDir + "/cfg/config.json"
 	_, err := os.Stat(confdir)
 
 	if err == nil {
 		//配置文件存在读取配置文件
 		cfgbytes, readfileerr := ioutil.ReadFile(confdir)
 		if readfileerr == nil {
+
 			filecontent := string(cfgbytes)
 			fmt.Println(filecontent)
+
+			//读取配置文件，并将配置文件转化到map中
+			mapconfig := make(map[string]interface{})
+			err = json.Unmarshal(cfgbytes, &mapconfig)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			//fmt.Println("json to map ", mapconfig)
+
+			for key, value := range mapconfig {
+				configvalue := fmt.Sprintf("%s", value)
+				AppConstant[key] = configvalue
+				//fmt.Printf(" the %s    and value is %s  \n", key, value)
+			}
+
+			/*for key,value := range mapconfig{
+							AppConstant[key]  = value
+			 			}*/
+
+			//fmt.Println("json to map ", mapconfig)
+			//fmt.Println("The value of key1 is", mapconfig["1"])
+
 		}
+
 	} else {
 
 		fmt.Println("没有发现配置文件 " + confdir)
@@ -41,8 +73,10 @@ func InitApp() {
 获取当前的系统路径
 */
 func GetCurrentPath() string {
+
 	s, _ := exec.LookPath(os.Args[0])
 	i := strings.LastIndex(s, "\\")
 	path := string(s[0 : i+1])
 	return path
+
 }
