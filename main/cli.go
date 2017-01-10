ea92009ab7067062ae7387c98f187907bed3535b
gcc2ge <wushouhe@hotmail.com>
2017-01-10 10:42:42 +0800

    flags

--- main/cli.go
+++ main/cli.go
@@ -1,4 +1,37 @@
 package main
 
+import (
+	"os"
+	"fmt"
+	"github.com/17golang/golang/cmd/utils"
+	"gopkg.in/urfave/cli.v1"
+	"github.com/17golang/golang/goutils/config"
+)
+
+var (
+	app = utils.NewApp("指旺命令行接口")
+	AppConstant map[string]interface{} = make(map[string]interface{})
+)
+
 func main() {
+	app.Action = action
+	app.AddAfter(func(*cli.Context) error {
+		fmt.Println("hhhhhhh")
+		return nil
+	})
+
+	app.AddFlag(cli.StringFlag{
+		Name:"config",
+		Usage:"设置配置文件",
+	})
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func action(ctx *cli.Context) error {
+	config.ReadConfig(ctx, "config", &AppConstant)
+	fmt.Println(AppConstant)
+	return nil
 }
